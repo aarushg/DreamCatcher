9bda50e1e0e422edf711a2faa3749277f032417e
AarushG <me@aarushg.com>
2023-07-16 23:51:44 -0400

    push to tree

--- App.js
+++ App.js
@@ -1,26 +1,13 @@
-import { StatusBar as ExpoStatusBar} from 'expo-status-bar';
-import { StatusBar, StyleSheet, SafeAreaView, Platform, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 
-
-
-console.log(StatusBar.currentHeight);
+import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
 
 export default function App() {
   return (
-      <> 
-      
-        <SafeAreaView style={{ flex: 1, marginTop: StatusBar.currentHeight }}>
-          <View style={{ padding:16, backgroundColor: "green"}}>
-            <Text>Search</Text>
-          </View>
-          <View style={{ flex: 1, padding: 16, backgroundColor: "blue"}}>
-            <Text>List</Text>
-          </View>
-        </SafeAreaView>
-        <ExpoStatusBar style="auto" />
-      </>
+    <>
+      <RestaurantsScreen />
+      <ExpoStatusBar style="auto" />
+    </>
   );
 }
-
-const styles = StyleSheet.create({});
