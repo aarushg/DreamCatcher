262c0d6479f261584dc85d0ccd1a9abf38ebfaea
AarushG <me@aarushg.com>
2023-07-16 23:58:39 -0400

    push up

--- App.js
+++ App.js
@@ -1,13 +1,36 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 import React from "react";
 
-import { RestaurantsScreen } from "./src/features/restaurants/screens/restaurants.screen";
+import { CountriesScreen } from "./src/features/countries/screens/countries.screen";
+import { ThemeProvider } from "styled-components/native";
+import { theme } from "./src/infastrcture/theme";
+
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+
 
 export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
+
   return (
     <>
-      <RestaurantsScreen />
-      <ExpoStatusBar style="auto" />
+      <ThemeProvider theme={theme}>
+        <CountriesScreen />
+        <ExpoStatusBar style="auto" />
+      </ThemeProvider>
     </>
   );
 }
