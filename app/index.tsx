5ff758226b4cbc640157d24a35ae9e38a1c5df64
AarushG <me@aarushg.com>
2025-05-05 09:59:09 -0400

    changes to the index and back up old index page

--- app/index.tsx
+++ app/index.tsx
@@ -1,38 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text } from 'react-native';
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
-import { SafeAreaView } from 'react-native-safe-area-context';
+const Home = () => {
 
-export default function HomeScreen() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text className={"text-red-500"}>DreamCatcher</Text>
-      <StatusBar style="auto" />
-    </SafeAreaView>
-  );
+    return(
+        <SafeAreaView>
+            <Text>Home</Text>
+        </SafeAreaView>
+    )
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    backgroundColor: '#fff',
-  },
-  titleContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+export default Home;
+
