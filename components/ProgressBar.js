5e9fbd0ee71d184cd7e7ca62c68b0c7127839a46
Aarush Gupta <aarush@aarushg.com>
2025-05-05 02:56:41 -0400

    new push setup

--- components/ProgressBar.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import * as Progress from 'react-native-progress';
-
-const ProgressBar = ({ progress, color }) => (
-  <Progress.Bar progress={progress} color={color} />
-);
-
-export default ProgressBar;
